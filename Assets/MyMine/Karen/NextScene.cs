e1f4d85c0ad57ebee61446666a8a74ef15472f4c
Tavo090 <31860551+Tavo090@users.noreply.github.com>
2020-02-02 13:45:26 -0600

    done

---
+++ Assets/MyMine/Karen/NextScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene : MonoBehaviour
+{
+    [SerializeField]
+    string nivelACargar;
+    [SerializeField]
+    public float retraso;
+
+    [ContextMenu("Activar Carga")]
+    public void ActivarCarga()
+    {
+        Invoke("CargarNivel", retraso);
+    }
+
+    void CargarNivel()
+    {
+        SceneManager.LoadScene(nivelACargar);
+    }
+}
